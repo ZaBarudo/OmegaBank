66da274153cc3fdb92e926fcc9b6d4a09fdaf960
Kevin <smartkevin2003@gmail.com>
2022-06-17 10:14:13 +0530

    fixed permit.sh again

--- permit.sh
+++ permit.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 
-chmod 770 OmegaBank
+chmod 771 OmegaBank
 setfacl -R -m g:CEO:rwx OmegaBank
+
 
 cd OmegaBank
 
 for branch in Branch1 Branch2 Branch3 Branch4
 do
-    setfacl -R -m g:"${branch}MGR":rwx ${branch}
+    sudo setfacl -R -m g:"${branch}MGR":rwx ${branch}
+    
     chmod 770 ${branch}
     cd ${branch}
-    for file in *
+    for file in $(ls . | head -n -3)
     do 
 
-        setfacl -m g:${file}:r-x ${file}
+        sudo setfacl -m g:"${file}":r-x ${file}
         chmod 770 ${file}
     done
     cd ..
 done
+
+cd ..
 
 
